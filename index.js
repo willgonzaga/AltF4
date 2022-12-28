521cdcabf39f6ec285ec8801f2c78acb0f1028e0
vailei <willey.oliveira12@gmail.com>
2022-12-28 20:37:31 -0300

    testando

--- index.js
+++ index.js
@@ -5,34 +5,38 @@ const { join } = require('path');
 
 app.use(express.static(join(__dirname + '/public')));
 app.get("/", function (req, res) {
+    const dotenv = require('dotenv');
+    dotenv.config();
+    const { MongoClient } = require('mongodb');
+    const url = process.env.linkDB;
+    const client = new MongoClient(url);
+    const dbName = 'AltF4';
+
+    async function main() {
+        // Use connect method to connect to the server
+        await client.connect();
+        console.log('Acessando banco de dados');
+        const db = client.db(dbName);
+        const collection = db.collection('Noticias');
+  
+        var teste = await collection.find({}).toArray();
+        console.log(teste)
+  
+        return 'done.';
+    }
+  
+    main()
+        .then(console.log)
+        .catch(console.error)
+        .finally(() => client.close());
     res.sendFile(__dirname + "/public/html/index.html");
+})
+app.get("/teste", function (req, res) {
+    var teste = "teste funfa";
+    console.log(teste)
+    res.sendFile(__dirname + "/public/html/index2.html");
 })
 
 app.listen(3000, function() {
     console.log("http://localhost:3000")
 })
-
-const dotenv = require('dotenv');
-dotenv.config();
-const { MongoClient } = require('mongodb');
-const url = process.env.linkDB;
-const client = new MongoClient(url);
-const dbName = 'AltF4';
-
-async function main() {
-    // Use connect method to connect to the server
-    await client.connect();
-    console.log('Acessando banco de dados');
-    const db = client.db(dbName);
-    const collection = db.collection('Noticias');
-  
-    var teste = await collection.find({}).toArray();
-    console.log(teste)
-  
-    return 'done.';
-}
-  
-  main()
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => client.close());
