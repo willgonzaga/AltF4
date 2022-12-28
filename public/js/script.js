8c62af55cd52b74cc8b1b9204d47ce0f768125a6
vailei <willey.oliveira12@gmail.com>
2022-12-28 19:19:02 -0300

    Testando

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const { MongoClient } = require('mongodb');
+const url = process.env.linkDB;
+const client = new MongoClient(url);
+const dbName = 'AltF4';
+
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('Noticias');
+  
+    // the following code examples can be pasted here...
+  
+    return 'done.';
+  }
+  
+  main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
